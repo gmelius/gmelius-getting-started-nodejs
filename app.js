d6f501f0a305d2c06245fd2fb13c6e12e945c4b4
td391 <david@gmelius.com>
2022-05-11 12:22:18 +0200

    fix: escape error messages

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ if (process.env.NODE_ENV !== 'production') {
 const board = require('./utils/board');
 const authentication = require('./utils/authentication');
 const generators = require('./utils/generators');
-const fs = require('fs')
-const https = require('https')
+const fs = require('fs');
+const escape = require('lodash.escape');
+const https = require('https');
 const express = require('express');
 
 
@@ -40,8 +41,12 @@ app.get("/callback", function (request, response) {
   const { code } = request.query;
   return authentication.getCallback(code, CODE_VERIFIER)
     .then(json => { ACCESS_TOKEN = json.access_token; return json; })
-    .then(json => json.error ? response.send(json.error) : response.redirect('/board'))
-    .catch(error => { console.log('error', error); response.send(error.message); });
+    .then(json => json.error ? response.send(escape(json.error)) : response.redirect('/board'))
+    .catch(error => {
+      console.log('error', error);
+      const escapedMessage = escape(error.message);
+      response.send(escapedMessage);
+    });
 })
 
 // board route
@@ -49,7 +54,11 @@ app.get('/board', (request, response) => {
   console.log(`GET '/board' 🤠 ${Date()}`);
   return board.getBoard(ACCESS_TOKEN)
     .then(({ board, columns, cards }) => response.render('board', { board, columns, cards }))
-    .catch(error => { console.log('error', error); response.send(error.message); });
+    .catch(error => {
+      console.log('error', error);
+      const escapedMessage = escape(error.message);
+      response.send(escapedMessage);
+    });
 });
 
 
